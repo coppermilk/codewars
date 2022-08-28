d012b34312c77f13fa0b3771b4be9c7a10f4de28
coppermilk <coppermilk@coppermilk.org>
2022-08-28 12:50:01 +0200

    Done "Duplicate Encoder".

---
+++ <6 kyu>/ Duplicate Encoder .cc
@@ -0,0 +1,37 @@
+#include <string>
+#include <climits>
+#include <iostream>
+
+std::string duplicate_encoder(const std::string &word) {
+    std::string encode_word;
+    size_t character_count[CHAR_MAX] = {0};
+
+    for (auto &c: word) {
+        ++character_count[std::tolower(c)];
+    }
+    for (auto &c: word) {
+        if (character_count[::tolower(c)] > 1) {
+            encode_word.push_back(')');
+        } else {
+            encode_word.push_back('(');
+        }
+    }
+
+    return encode_word;
+}
+
+int main() {
+
+    // "din"      =>  "(((".
+    // "recede"   =>  "()()()".
+    // "Success"  =>  ")())())".
+    // "(( @"     =>  "))((".
+
+    std::cout << duplicate_encoder("din") << std::endl;
+    std::cout << duplicate_encoder("recede") << std::endl;
+    std::cout << duplicate_encoder("Success") << std::endl;
+    std::cout << duplicate_encoder("(( @") << std::endl;
+
+
+    return 0;
+}
