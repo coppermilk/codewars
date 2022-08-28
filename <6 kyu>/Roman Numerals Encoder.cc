66289014efeefa1f787bfecded3aba7b53141bcd
coppermilk <coppermilk@coppermilk.org>
2022-08-28 16:07:05 +0200

    start new kata

--- <6 kyu>/Roman Numerals Encoder.cc
+++ <6 kyu>/Roman Numerals Encoder.cc
@@ -11,10 +11,26 @@ std::string solution(int number) {
 //    C          100
             //    D          500
     while(number){
-        int X = number % 10;;
-
+        int X = number / 10;;
+        for(int i = X; X > 0; --X){
+            ans.push_back('X');
+            number -= 10;
+        }
+        int V = number / 5;
+        for(int i = V; V > 0; --V){
+            ans.push_back('V');
+            number-=5;
+        }
+        int I = number;
+        int k =number -
+        for(int i = I; I > 0; --I){
+            ans.push_back('I');
+            --number;
+        }
+        break;
     }
     return ans;
+
 }
 
 int main() {
