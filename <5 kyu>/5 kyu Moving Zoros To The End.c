5249f1fa5aaba1eb13ab27c1c77dd2f7385e31a0
coppermilk <coppermilk@coppermilk.org>
2022-08-29 23:58:41 +0200

    Refactor "Moving Zeros to the End".

--- <5 kyu>/5 kyu Moving Zoros To The End.c
+++ <5 kyu>/5 kyu Moving Zoros To The End.c
@@ -1,48 +1,19 @@
 // [Kata] https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/c
 // [User] https://www.codewars.com/users/coppermilk
-// NEED TO REFACTOR !!!!!!!
+
 #include <stddef.h>
-#include <stdlib.h>
 #include <stdio.h>
 
 void move_zeros(size_t len, int arr[len]) {
-    int is_all_zero = 0;
-    for (size_t i = 0; i < len; i++) {
-        for (size_t j = i; (j < len - 1) && abs(arr[j + 1]) >= arr[j] && (arr[j] == 0); j++) {
-
-            size_t tmp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = tmp;
-
-
-            //std::swap(arr[j], arr[j+1]);
-            printf("i%d : j%d : ", i, j);
-            for (int i = 0; i < 20; ++i) {
-                printf("%d", arr[i]);
-            }
-            if (arr[i] == 0 && arr[i + 1] == 0) {
-
-                int all_sum = 0;
-                for (int k = i; k < len; k++) {
-                    all_sum += arr[k];
-                }
-                if (all_sum == 0) {
-                    is_all_zero = 1;
-                }
-                --i;
-            }
-
-            printf("\n");
-            if (is_all_zero) {
-                break;
-            }
-        }
-
-        printf("\n");
-        if (is_all_zero) {
-            break;
-        }
-    }
+   for(int i = 0; i < len; i++){
+       for(int j = 0; j < i; j++){
+           if((i > j) && arr[j] == 0 && arr[i] != 0){
+               size_t buf = arr[i];
+               arr[i] = arr[j];
+               arr[j] = buf;
+           }
+       }
+   }
 }
 
 int main() {
