8609c5a34a24dd93cd26e7dfb4e29546993d6862
coppermilk <coppermilk@coppermilk.org>
2022-08-31 22:02:10 +0200

    Progress "Valid Parentheses".

--- <5 kyu>/5 kyu Directions Reduction.cc
+++ <5 kyu>/5 kyu Directions Reduction.cc
@@ -1,44 +1,45 @@
+// UNCOMPLETED
 #include <string>
-#include <vector>
+
 #include <iostream>
+#include <array>
+#include <vector>
+#include <map>
 
-class DirReduction {
+class DirReduction
+{
 public:
-    static std::vector<std::string> dirReduc(std::vector<std::string> &arr) {
-        // Compass.
-        //    N         +
-        //  W-*-E     - * +
-        //    S         -
+    static std::vector<std::string> dirReduc(std::vector<std::string> &arr)
+    {
+        if (arr.size() <= 1) return arr;
 
-        // Remove extra direction.
-        bool is_chenge = false;
-        std::vector<std::string> ans;
-        for (int i = 0; i < arr.size() - 1; ++i) {
-            if (arr[i] == "WEST" && arr[i + 1] == "EAST" ||
-                arr[i + 1] == "WEST" && arr[i] == "EAST") {
-                arr[i] = arr[i + 1] = "";
-                is_chenge =true;
-            } else if (arr[i] == "NORTH" && arr[i + 1] == "SOUTH" ||
-                       arr[i + 1] == "NORTH" && arr[i] == "SOUTH") {
-                arr[i] = arr[i + 1] = "";
-                is_chenge = true;
+        std::vector<std::string> res = arr;
+        std::map<std::string, std::Coord> coords {
+                { "NORTH", {  0,  1 } },
+                { "SOUTH", {  0, -1 } },
+                { "WEST" , { -1,  0 } },
+                { "EAST" , {  1,  0 } }
+        };
+
+        for (unsigned i = 1, x = 0, y = 1; i < res.size(); ++i)
+        {
+            std::string pos1 = res[i - 1], pos2 = res[i];
+
+            bool north_south = coords[pos1][x] == coords[pos2][x] && coords[pos1][y] != coords[pos2][y];
+            bool west_east = coords[pos1][y] == coords[pos2][y] && coords[pos1][x] != coords[pos2][x];
+            bool is_opposite_directions = north_south || west_east;
+
+            if (is_opposite_directions)
+            {
+                res.erase(res.begin() + (i - 1), res.begin() + (i + 1));
+                i = 0;
             }
         }
 
-        // Add necessary direction.
-        for (auto &s: arr) {
-            if (!s.empty()) {
-                ans.push_back(s);
-            }
-        }
-        if(!is_chenge){
-            return ans;
-        }else{
-            return dirReduc(ans);
-        }
-
+        return res;
     }
 };
+
 
 int main() {
     std::vector<std::string> test1 = {"WEST", "EAST", "NORTH", "SOUTH", "EAST", "NORTH", "SOUTH", "NORTH", "SOUTH",
