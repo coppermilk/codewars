984413ed34c3ec5381d9561f9a1996fb72f931d6
coppermilk <coppermilk@coppermilk.org>
2022-09-01 23:01:33 +0200

    Done "Rot13".

---
+++ <5 kyu>/5 kyu Rot13.cc
@@ -0,0 +1,33 @@
+// [Kata] https://www.codewars.com/kata/530e15517bc88ac656000716/train/cpp
+// [User] https://www.codewars.com/users/coppermilk
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string rot13(string msg) {
+    const char shift = 13;
+    for (char &ch: msg) {
+        if ('a' <= ch && ch <= 'z') {
+            if (ch > 'z' - shift) {
+                ch = (char) ('a' + (ch - 'a' - shift));
+            } else {
+                ch = (char) (ch + shift);
+            }
+        } else if ('A' <= ch && ch <= 'Z') {
+            if (ch > 'Z' - shift) {
+                ch = (char) ('A' + (ch - 'A' - shift));
+            } else {
+                ch = (char) (ch + shift);
+            }
+        }
+
+    }
+    return msg;
+}
+
+int main() {
+    std::cout << rot13("abcdefghijklmnopqrstuvwxyz");
+
+}
--- <5 kyu>/5 kyu Simple Pig Latin.cc
+++ <5 kyu>/5 kyu Simple Pig Latin.cc
@@ -1,3 +1,6 @@
+// [Kata] https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/cpp
+// [User] https://www.codewars.com/users/coppermilk
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -13,26 +16,40 @@ std::string pig_it(std::string str) {
     while (ss >> word) {
         words.push_back(word);
     }
-    //Swap letter and add ending.
+
+    // Find end word.
     for (auto &w: words) {
-        w += w[0];
-        w.erase(w.begin());
-        if(isalpha(w[w.size()-1])){
-            w += "ay";
+        unsigned long end_index = 0;
+        while (!std::isalpha(w[w.size() - end_index]) && end_index < w.size()) {
+            ++end_index;
         }
 
+        // Change the end of a word and remove first letter.
+        std::string tmp;
+        tmp.push_back(w[0]);
+        if(std::isalpha(w[0])){
+            tmp += "ay";
+        }
+        w.insert(w.size() - end_index + 1, tmp);
+        w.erase(w.begin());
+
     }
+
     // Construct answer.
     std::string ans;
-    for (auto &w: words) {
-        if (words.size() - 1)
-            ans.append(  w + " ");
+    for(unsigned long i = 0; i < words.size(); ++i){
+        if(i == words.size() -1){
+            ans += words[i];
+        }else{
+            ans = (ans + words[i] + " ");
+        }
     }
-    ans.erase(ans.end() - 1);
-
     return ans;
+
 }
 
 int main() {
-    std::cout << pig_it(" ou.,ek;ke'ke?!"); // "hisTay siay ymay tringsay" .,;"'?!od
+    // Should return "histay. histay, histay; e'kekay? histay! ;";
+    std::cout << pig_it("this. this, this; ke'ke? this! ;");
+
 }
