293131bb0170e760d0bf1608008327ab6de3aea8
coppermilk <coppermilk@coppermilk.org>
2022-09-09 00:15:08 +0200

    Done "filling an array part 1".

--- <8 kyu>/8_kyu_filling_an_array_part_1.cc
+++ <8 kyu>/8_kyu_filling_an_array_part_1.cc
@@ -2,19 +2,30 @@
 // [User] https://www.codewars.com/users/coppermilk
 
 #include <vector>
+#include <numeric>
 
+// First solution.
 std::vector<int> arr(int n = 0) {
     /*Produces an array with the numbers 0 to n-1.*/
 
     std::vector<int> v;
     v.resize(n);
-    for(int i = 0; i < n; ++i){
+    for (int i = 0; i < n; ++i) {
         v[i] = i;
     }
     return v;
 }
 
-int main(){
-    arr(42);
+//Second solution.
+std::vector<int> arr2(int n = 0) {
+    /*Produces an array with the numbers 0 to n-1.*/
+
+    std::vector<int> v(n);
+    std::iota(v.begin(), v.end(), 0);
+    return v;
+}
+
+int main() {
+    arr2(42);
     return 0;
 }
