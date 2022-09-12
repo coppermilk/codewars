a23f92018e198b57307858f8f8e66b125aec147e
coppermilk <coppermilk@coppermilk.org>
2022-09-12 18:04:41 +0200

    Done "8 kyu katas".

--- <8 kyu>/8_kyu_convert_a_string_to_an_array.c
+++ <8 kyu>/8_kyu_convert_a_string_to_an_array.c
@@ -1,54 +1,47 @@
-//{to do
+// [Kata] https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/c
+// [User] https://www.codewars.com/users/coppermilk
+// 8 kyu Convert a string to an array.
+
 #include <stdlib.h>
 #include <string.h>
 
+size_t count_words(const char *words) {
+    /* Count the number of words.
+       This function will be used to allocate the right amount of memory so it has to be correct !*/
 
-size_t count_words (const char *words)
-{
-
-    int i = 0;
-    int wrd = 1;
-    if(!words){
+    int w = 1;
+    if (*words) {
+        while (*words) {
+            if (*words == ' ') w++;
+            words++;
+        }
+        return w;
+    }
+    else{
         return 0;
     }
-    while (words[i]){
-        if(words[i] == ' ' || words[i] == '\n' || words[i] == '\t'){
-            ++wrd;
+}
 
+void words_to_array(char *words, char *words_array[]) {
+    /*Split a string and convert it into an array of words.*/
+
+    words_array[0] = calloc(1, sizeof(char));
+    int i = 0;
+    int w_s = 0;
+    while (*words) {
+        if (*words == ' ') {
+            i++;
+            words_array[i] = calloc(1, sizeof(char));
+            w_s = 0;
+        } else {
+            words_array[i][w_s] = *words;
+            w_s++;
         }
-        ++i;
+        ++words;
     }
-// count the number of words
-// this function will be used to allocate the right amount of memory so it has to be correct !
-    return wrd;
 }
 
-void words_to_array (char *words, char *words_array[])
-{
-    printf("%s\n", words);
-    if(!words[0]){
-        //memset(words_array, 0, 1);
-        words_array[0] = NULL;
-        return;
-    }
-
-    char * token = strtok(words, " ");
-
-    int i=0;
-    int w = count_words(words);
-    int w_i = 0;
-//walk through other tokens
-    while(token != NULL && w_i < w) {
-
-        words_array[i] = calloc(1, strlen(token) + 1);
-        strncpy(words_array[i], token, strlen(token));
-        i++;
-        token = strtok(NULL, " ");
-        w_i++;
-    }
-
-}
-int main(){
-    char* k[1];
+int main() {
+    char *k[1];
     words_to_array("", k);
 }
