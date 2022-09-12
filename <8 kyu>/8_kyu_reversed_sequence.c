8e5f9977b506654709ce5524d29a1b14d04dbefb
Mr. Coppermilk <lawrence.artem@gmail.com>
2022-09-12 19:56:08 +0200

    Update 8_kyu_reversed_sequence.c

--- <8 kyu>/8_kyu_reversed_sequence.c
+++ <8 kyu>/8_kyu_reversed_sequence.c
@@ -7,7 +7,7 @@
 unsigned short *reverse_seq(unsigned short num){
     /*Build a function that returns an array of integers from n to 1 where n>0.*/
 
-    unsigned short * arr = calloc(num +1, sizeof(unsigned short));
+    unsigned short * arr = arr = malloc(num * sizeof(unsigned short));
     int j = 0;
     for(int i = num; i > 0; --i){
         *(arr + j++) = i;
