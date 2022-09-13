a3dd4a8f63373c2e13f5776cac374c2cad090b42
coppermilk <coppermilk@coppermilk.org>
2022-09-13 16:16:52 +0200

    Done "8 kyu katas".

--- <8 kyu>/8_kyu_ pythagorean_triple.c
+++ <8 kyu>/8_kyu_ pythagorean_triple.c
@@ -1,22 +1,17 @@
 // [Kata] https://www.codewars.com/kata/5951d30ce99cf2467e000013/train/c
 // [User] https://www.codewars.com/users/coppermilk
 // 8 kyu Pythagorean Triple
-// To do
 
 #include <stdbool.h>
 
 bool pythagorean_triple(const unsigned sides[3]) {
     /*Check pythagorean triple.*/
+
     unsigned long long a = sides[0];
     unsigned long  long b = sides[1] ;
     unsigned long long  c = sides[2];
 
-    a = a * a;
-    b = b * b;
-    c = c * c;
-    unsigned long long ab = a + b;
-
-    return a + b == c;
+    return a*a==b*b+c*c || b*b==a*a+c*c || c*c==a*a+b*b;
 }
 
 int main(){
